3508cc417be6df7ad83cf6569d67662a119116de
NOIACAST-SC <137130349+NOIACAST-SC@users.noreply.github.com>
2023-06-23 23:26:58 -0300

    portuguese-br translation

---
+++ languages/ptbr_lang.lua
@@ -0,0 +1,34 @@
+--------------------------------------------------------------------------------------
+---------------------------------- Portugues-BR -------------------------------------------
+
+Locales["ptbr_lang"] = {
+
+    SubPrompt = "Pressionar",
+    Sellitem = "Vender Item",
+    SubMenu = "Menu",
+    choose_category = "Escolha uma categoria",
+    sellmenu = "menu de venda",
+    buymenu = "menu de compra",
+    sellfor = "VENDER POR",
+    buyfor = "COMPRAR POR",
+    youdontsell = "você não tem o item para vender!",
+    chooseoption = "Escolha uma opção",
+    amount = "QUANTIDADE",
+    confirm = "Confirmar",
+    insertamount = "insira uma quantidade!",
+    must = "deve conter apenas de 1 a 20 números.",
+    yousold = "Você vendeu ",
+    fr = " por ",
+    frcash = " por $",
+    ofcash = " dinheiro",
+    ofgold = "  ouro",
+    youbought = "Você comprou ",
+    youdontgold = "você não tem ouro suficiente",
+    youdontcash = "você não tem dinheiro suficiente",
+    cantcarry = "você não pode carregar este item",
+    closed = "FECHADO o",
+    am = ":00 da manhã até o",
+    pm = ":00 da tarde",
+    limitSell = "Eu não compro mais tantos desse item",
+    limitBuy = "Infelizmente, não tenho mais tanto estoque desse item"
+}
